6ad848bb8cfd89e67868a3b2432ca0fd0a8dceab
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-25 18:03:15 -0300

    Initial header component and SSR

---
+++ header/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <header>
+      <h1>Logo</h1>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default App;
---
+++ header/transpiled/App.js
@@ -0,0 +1,43 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function App() {
+  return _react2.default.createElement(
+    'header',
+    null,
+    _react2.default.createElement(
+      'h1',
+      null,
+      'Logo'
+    ),
+    _react2.default.createElement(
+      'nav',
+      null,
+      _react2.default.createElement(
+        'ul',
+        null,
+        _react2.default.createElement(
+          'li',
+          null,
+          'About'
+        ),
+        _react2.default.createElement(
+          'li',
+          null,
+          'Contact'
+        )
+      )
+    )
+  );
+}
+
+exports.default = App;
