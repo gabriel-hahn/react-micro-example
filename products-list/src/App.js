6250cd6bdc51082ad1b201877d31a46a4bc6b174
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-05-25 18:56:23 -0300

    Product lists project created

---
+++ products-list/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <header>
+      Products List
+    </header>
+  );
+}
+
+export default App;
---
+++ products-list/transpiled/App.js
@@ -0,0 +1,21 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function App() {
+  return _react2.default.createElement(
+    'header',
+    null,
+    'Products List'
+  );
+}
+
+exports.default = App;
